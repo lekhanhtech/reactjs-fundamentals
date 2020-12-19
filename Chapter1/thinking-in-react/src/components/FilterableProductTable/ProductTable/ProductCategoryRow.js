b08e71fe69ccb081eef096ba45a3641a59df805b
Le Khanh <lekhanh.org.vn@gmail.com>
2020-12-19 13:16:50 +0700

    + demo for lesson 7

---
+++ Chapter1/thinking-in-react/src/components/FilterableProductTable/ProductTable/ProductCategoryRow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default class ProductCategoryRow extends React.Component {
+    render() {
+      const category = this.props.category;
+      return (
+        <tr>
+          <th colSpan="2">
+            {category}
+          </th>
+        </tr>
+      );
+    }
+}
---
+++ Chapter1/thinking-in-react/src/components/FilterableProductTable/ProductTable/ProductRow.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class ProductRow extends React.Component {
+    render() {
+      const product = this.props.product;
+      const name = product.stocked ?
+        product.name :
+        <span style={{color: 'red'}}>
+          {product.name}
+        </span>;
+  
+      return (
+        <tr>
+          <td>{name}</td>
+          <td>{product.price}</td>
+        </tr>
+      );
+    }
+}
