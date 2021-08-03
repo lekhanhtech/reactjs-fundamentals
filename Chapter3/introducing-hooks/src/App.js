170a3c024c82f5735c1d2055ee605caf260cbddd
Le Khanh <lekhanh.org.vn@gmail.com>
2021-08-03 18:09:29 +0700

    + add demo for chapter 3

---
+++ Chapter3/introducing-hooks/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Example from './Example'
+
+function App() {
+  return (
+    <div className="App">
+      <Example />
+    </div>
+  );
+}
+
+export default App;
